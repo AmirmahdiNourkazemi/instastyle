80523c9d93c979cc3896aeba57bf91fca8b72214
AmirmahdiNourkazemi <nourkazemi80@gmail.com>
2025-08-29 22:43:30 +0330

    feat: web

--- lib/feature/home/presentation/widgets/export_class.dart
+++ lib/feature/home/presentation/widgets/export_class.dart
@@ -2,7 +2,9 @@ import 'dart:typed_data';
 import 'dart:ui' as ui;
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
-import 'package:super_clipboard/super_clipboard.dart'; // Add this in pubspec.yaml
+import 'package:instastyle/core/widgets/show_toast.dart';
+import 'package:super_clipboard/super_clipboard.dart';
+import 'package:toastification/toastification.dart'; // Add this in pubspec.yaml
 
 class Export {
   final BuildContext context;
@@ -24,8 +26,11 @@ class Export {
       item.add(Formats.png(pngBytes));
       await ClipboardWriter.instance.write([item]);
 
+      showToast(context, 'کپی شد', '', ToastificationType.success,
+          isIconMessage: true);
+
       ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text("📋 Copied image to clipboard!")),
+        const SnackBar(content: Text("📋 کپی شد")),
       );
     } catch (e) {
       ScaffoldMessenger.of(context).showSnackBar(
